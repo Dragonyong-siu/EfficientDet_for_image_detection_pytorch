2ea8cae24ed1744705bbe343a80851fe5995487e
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-12-03 21:25:36 +0900

    Update EfficientDet_Fit.py

--- EfficientDet_Fit.py
+++ EfficientDet_Fit.py
@@ -2,15 +2,22 @@
 
 def EfficientDet_Fit(train_function, 
                      dataloader, 
+                     size, 
+                     encoder,
                      model, 
                      loss_class1, 
                      loss_class2, 
                      epoches, 
                      learning_rate):
 
-  optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate)
+  optimizer = torch.optim.SGD(model.parameters(),
+                              lr = learning_rate,
+                              momentum = 0.9,
+                              weight_decay = 4e-3)
+  
+  scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience = 3, verbose = True)
   for i in range(epoches):
     print(f"epoches:{i+1}")
     print('train')
-    train_function(dataloader, model, loss_class1, loss_class2, optimizer)
-    torch.save(model, '/content/gdrive/My Drive/' + f'EfficientDet_Model:{i+1}')
+    train_function(dataloader, size, encoder, model, loss_class1, loss_class2, optimizer, scheduler)
+  torch.save(model, '/content/gdrive/My Drive/' + f'EfficientDet_Model:{i+1}')
